3862001d1a6ba43caafa43eb117133fe2f6bb711
Hazelin Ngan <hazelin.ngan@gmail.com>
2017-09-10 02:27:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,7 +46,14 @@ switch(itemsAndPrices.length){
 }
 
 function total() {
-  // write your code here
+  let t = 0;
+
+  for (let i=0, l=cart.length; i<l; i++){
+    for (let item in cart[i]){
+      t += cart[i][item];
+    }
+  }
+  return t;
 }
 
 function removeFromCart(item) {
